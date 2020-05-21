cec1047c6011e9505678ea5ebc4b959b5d274956
Kirill Levitskiy <40201204+kirill-levitskiy@users.noreply.github.com>
2020-05-21 19:41:05 +0300

    update sailor to 2.6.7 (#2)
    * update sailor to 2.6.7

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+## 1.3.1 (May 22, 2020)
+
+* Update sailor version to 2.6.7
+
+## 1.3.0 (April 21, 2017)
+
+* Update sailor version to 2.1.0
+
+## 1.2.0 (February 21, 2017)
+
+* Update sailor version to 2.0.0
+
+## 1.1.0 (December 16, 2016)
+
+* Add Consume Trigger
+
+## 1.0.0 (December 15, 2016)
+
+* Initial release
+* Add Publish Action
--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,23 +1,24 @@
-'use strict';
 const co = require('co');
 const amqp = require('amqplib');
 
 // This function will be called by the platform to verify credentials
 module.exports = function verifyCredentials(credentials, cb) {
-  console.log('Credentials passed for verification %j', credentials);
-  const amqpURI = credentials.amqpURI;
+  const self = this;
+  this.logger.info('Credentials passed for verification %j', credentials);
+  const { amqpURI } = credentials;
   const amqpExchange = 'verify_credentials_test';
-  co(function*() {
-    console.log('Connecting to amqpURI=%s', amqpURI);
+  // eslint-disable-next-line func-names
+  co(function* () {
+    self.logger.info('Connecting to amqpURI=%s', amqpURI);
     const conn = yield amqp.connect(amqpURI);
-    console.log('Creating a confirm channel');
+    self.logger.info('Creating a confirm channel');
     const channel = yield conn.createConfirmChannel();
-    console.log('Asserting topic exchange exchange=%s', amqpExchange);
+    self.logger.info('Asserting topic exchange exchange=%s', amqpExchange);
     yield channel.assertExchange(amqpExchange, 'topic', { durable: false });
-    console.log('Verified successfully');
-    cb(null, {verified: true});
-  }).catch(err => {
-    console.log('Error occurred', err.stack || err);
-    cb(err , {verified: false});
+    self.logger.info('Verified successfully');
+    cb(null, { verified: true });
+  }).catch((err) => {
+    this.logger.info('Error occurred', err.stack || err);
+    cb(err, { verified: false });
   });
 };
